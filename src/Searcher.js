df5b96b60ae9a634ec92567c84a7636d175350d3
Joan Morató <moratojoan@gmail.com>
2021-05-23 16:34:38 +0200

    fetch municipios

--- src/Searcher.js
+++ src/Searcher.js
@@ -1,52 +1,37 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 
-import { EuiComboBox } from '@elastic/eui';
+import {
+    EuiComboBox,
+    EuiHighlight
+} from '@elastic/eui';
+
+import { getMunicipios } from './getMunicipios';
 
 import './Searcher.css';
 
 
-const options = [
-    {
-      label: 'Titan',
-      'data-test-subj': 'titanOption',
-    },
-    {
-      label: 'Enceladus',
-    },
-    {
-      label: 'Mimas',
-    },
-    {
-      label: 'Dione',
-    },
-    {
-      label: 'Iapetus',
-    },
-    {
-      label: 'Phoebe',
-    },
-    {
-      label: 'Rhea',
-    },
-    {
-      label:
-        "Pandora is one of Saturn's moons, named for a Titaness of Greek mythology",
-    },
-    {
-      label: 'Tethys',
-    },
-    {
-      label: 'Hyperion',
-    },
-  ];
-
 export default function Searcher() {
+    const [options, setOptions] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+
+        getMunicipios()
+        .then(municipios => {
+            console.log(municipios);
+            setOptions(municipios);
+            setIsLoading(false);
+        });
+    }, []);
 
     return (
         <div className="searcher-box">
             <EuiComboBox
-                placeholder="Select one or more options"
+                placeholder="Buscar municipio"
+                isLoading={isLoading}
                 options={options}
+                sortMatchesBy="startsWith"
             />
         </div>
     );
---
+++ src/getMunicipios.js
@@ -0,0 +1,41 @@
+
+
+
+export function getMunicipios() {
+    return new Promise(resolve => {
+        fetch("https://www.el-tiempo.net/api/json/v2/municipios")
+        .then(response => {
+            if(!response.ok) throw new Error(response.status);
+            return response.json();
+        })
+        .then(municipios => {
+            const municipiosTransformed = transformMunicipios(municipios);
+            resolve(municipiosTransformed);
+        })
+        .catch(error => console.log(error));
+    });
+}
+
+function transformMunicipios(municipios) {
+    return municipios.map(transformMunicipio);
+}
+
+function decodeApostroph(string) {
+    const APOSTROPH_ENTITY = "&#39;";
+    const stringDecoded = string.replaceAll(APOSTROPH_ENTITY, "'");
+    return stringDecoded;
+}
+
+function transformMunicipio(municipio) {
+    const getId = ({CODIGOINE}) => CODIGOINE.slice(0, 5);
+    const getLabel = ({NOMBRE, NOMBRE_PROVINCIA}) =>
+        `${decodeApostroph(NOMBRE)} - ${decodeApostroph(NOMBRE_PROVINCIA)}`;
+
+    return {
+        label: getLabel(municipio),
+        value: {
+            id: getId(municipio),
+            codProv: municipio.CODPROV,
+        }
+    }
+}
