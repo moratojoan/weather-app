99bed0c4afcb1270843477e951323eb5692d8031
Joan Morató <moratojoan@gmail.com>
2021-05-24 15:32:27 +0200

    set, get and remove municipos from firebase db

--- src/App.js
+++ src/App.js
@@ -1,15 +1,63 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Page from './Page';
 import Searcher from './Searcher';
 import Auth from './Auth';
 import Cards from './Cards';
 
+import 'firebase/database';
+import {
+    useFirebaseApp,
+    useUser
+} from 'reactfire';
+
+
+function reformatMunicipiosFromDB(municipiosFromDB) {
+    return  Object.entries(municipiosFromDB).map(([id, {name, codProv}]) => ({
+        id,
+        name,
+        codProv
+    }));
+}
 
 export default function App() {
+    const firebase = useFirebaseApp();
+    const user = useUser();
     const [municipiosSelected, setMunicipiosSelected] = useState([]);
 
+    useEffect(() => {
+        if(!user.data) return;
+
+        firebase.database()
+        .ref(`users/${user.data.uid}/municipios`)
+        .get()
+        .then(municipiosFromDB => {
+            if(municipiosFromDB.exists()) {
+                const municipiosReformated = reformatMunicipiosFromDB(municipiosFromDB.val());
+                setMunicipiosSelected(municipiosReformated);
+            }
+        });
+    }, [firebase, user.data]);
+
+    const getMunicipioRefFromDB = municipioId => {
+        return firebase.database().ref(`users/${user.data.uid}/municipios/${municipioId}`);
+    }
+    const setMuniciposSelectedToDB = value => {
+        if(!user.data) return;
+
+        getMunicipioRefFromDB(value.id).set({
+            name: value.name,
+            codProv: value.codProv
+        });
+    }
+    const removeMunicipioFromDB = municipioToDelteId => {
+        if(!user.data) return;
+
+        getMunicipioRefFromDB(municipioToDelteId).remove();
+    }
+
     const onSelectMunicipio = async ([{value}]) => {
+        setMuniciposSelectedToDB(value);
         setMunicipiosSelected([
             ...municipiosSelected,
             value
@@ -17,6 +65,7 @@ export default function App() {
     }
 
     const onDeleteCard = municipioToDelteId => {
+        removeMunicipioFromDB(municipioToDelteId);
         setMunicipiosSelected(municipiosSelected.filter(m => m.id !== municipioToDelteId))
     }
 
