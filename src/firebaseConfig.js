d04ae043855d63e91580e75569979c2121331da2
Joan Morató <moratojoan@gmail.com>
2021-05-24 13:15:21 +0200

    initialize firebase realtime database

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,6 +3,7 @@ export const firebaseConfig = {
     apiKey: "AIzaSyAzkcz290i1kEgXFuylJ9UGtZ0unDzPKvA",
     authDomain: "iomed-weather.firebaseapp.com",
     projectId: "iomed-weather",
+    databaseURL: "https://iomed-weather-default-rtdb.europe-west1.firebasedatabase.app/",
     storageBucket: "iomed-weather.appspot.com",
     messagingSenderId: "351028993823",
     appId: "1:351028993823:web:7f342293eba485f1359a2a",
